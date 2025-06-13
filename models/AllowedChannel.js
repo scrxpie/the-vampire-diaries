c99723f3a65d19627cf025ac41a3ed06dcc3248d
ixaryl <rahimasim15@gmail.com>
2025-06-13 12:11:37 +0400

    Update AllowedChannel.js

--- models/AllowedChannel.js
+++ models/AllowedChannel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const allowedChannelSchema = new mongoose.Schema({
-  channelId: { type: String, required: true }
+  channelId: { type: String, required: true, unique: true },
+  type: { type: String, enum: ['channel', 'category'], required: true }
 });
 
 module.exports = mongoose.model('AllowedChannel', allowedChannelSchema);
