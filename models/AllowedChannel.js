b798046dc8328a03cf01c37431c4484d857e3025
ixaryl <rahimasim15@gmail.com>
2025-06-13 11:46:51 +0400

    Update AllowedChannel.js

--- models/AllowedChannel.js
+++ models/AllowedChannel.js
@@ -1,1 +1,7 @@
+const mongoose = require('mongoose');
 
+const allowedChannelSchema = new mongoose.Schema({
+  channelId: { type: String, required: true }
+});
+
+module.exports = mongoose.model('AllowedChannel', allowedChannelSchema);
