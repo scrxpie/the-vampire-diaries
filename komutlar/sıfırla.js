e60a303e3a8b29ac37cc30d3d0ee62452c4a982c
ixaryl <rahimasim15@gmail.com>
2025-06-13 11:40:00 +0400

    Update sıfırla.js

--- komutlar/sıfırla.js
+++ komutlar/sıfırla.js
@@ -1,27 +1,20 @@
-const fs = require('fs');
+const WeeklyWords = require('../models/WeeklyWords'); // Yolunu ayarla
 
 module.exports = {
   name: 'sıfırla',
   description: 'Haftalık kelime verilerini sıfırlar.',
   async execute(message) {
-    const filePath = './data/haftalikKelimeVerisi.json';
-
-    // Kullanıcıların sıfırlama komutunu kullanabilmesi için yetki kontrolü
     if (!message.member.permissions.has('ADMINISTRATOR')) {
       return message.reply('Bu komutu kullanmak için yönetici yetkisine sahip olmalısınız.');
     }
 
-    // Eğer dosya mevcut değilse, mesajla kullanıcıyı bilgilendiriyoruz
-    if (!fs.existsSync(filePath)) {
-      return message.reply('Haftalık kelime veritabanı bulunamadı.');
-    }
-
-    // JSON dosyasını sıfırlama işlemi
     try {
-      fs.writeFileSync(filePath, JSON.stringify({}, null, 2));
+      // Tüm weekly kelimeleri sıfırla
+      await WeeklyWords.updateMany({}, { $set: { words: 0 } });
+
       return message.reply('Haftalık kelime verileri başarıyla sıfırlandı.');
     } catch (error) {
-      console.error('Dosya sıfırlama hatası:', error);
+      console.error('Veri sıfırlama hatası:', error);
       return message.reply('Veri sıfırlama sırasında bir hata oluştu.');
     }
   },
