f8797c2d0fd29fa5ee00a19b8db1ad3000996896
ixaryl <rahimasim15@gmail.com>
2025-06-16 14:34:27 +0400

    Update maaş-ver.js

--- komutlar/maaş-ver.js
+++ komutlar/maaş-ver.js
@@ -2,7 +2,7 @@ const Salary = require('../models/Salary');
 
 module.exports = {
   name: 'maaş-ver',
-  description: 'Kullanıcının maaş engelini kaldırır.',
+  description: 'Kullanıcının maaş alma hakkını geri verir.',
   async execute(message, args) {
     if (!message.member.permissions.has('ADMINISTRATOR')) {
       return message.reply('Bu komutu kullanmak için yönetici olmalısın.');
@@ -11,12 +11,12 @@ module.exports = {
     const user = message.mentions.users.first();
     if (!user) return message.reply('Bir kullanıcı etiketlemelisin.');
 
-    await Salary.findByIdAndUpdate(
-      user.id,
-      { salaryBlocked: false },
+    await Salary.updateOne(
+      { _id: user.id },
+      { $set: { salaryBlocked: false } },
       { upsert: true }
     );
 
-    message.reply(`${user.username} artık bu hafta maaş alabilir.`);
+    message.reply(`${user.username} adlı kullanıcının bu haftaki maaş alım hakkı yeniden aktif edildi.`);
   }
 };
