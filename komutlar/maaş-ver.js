0bcfcfedeecc52301e3f9b4faaaaed810dc90c40
ixaryl <rahimasim15@gmail.com>
2025-06-16 14:45:51 +0400

    Update maaş-ver.js

--- komutlar/maaş-ver.js
+++ komutlar/maaş-ver.js
@@ -1,22 +1,32 @@
+const { PermissionsBitField } = require('discord.js');
 const Salary = require('../models/Salary');
 
 module.exports = {
   name: 'maaş-ver',
-  description: 'Kullanıcının maaş alma hakkını geri verir.',
+  description: 'Bir kullanıcının maaş alma hakkını açar.',
   async execute(message, args) {
-    if (!message.member.permissions.has('ADMINISTRATOR')) {
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
       return message.reply('Bu komutu kullanmak için yönetici olmalısın.');
     }
 
     const user = message.mentions.users.first();
-    if (!user) return message.reply('Bir kullanıcı etiketlemelisin.');
+    if (!user) {
+      return message.reply('Lütfen maaşı açılacak kullanıcıyı etiketle.');
+    }
 
-    await Salary.updateOne(
-      { _id: user.id },
-      { $set: { salaryBlocked: false } },
-      { upsert: true }
-    );
+    let salaryData = await Salary.findOne({ userId: user.id });
+    if (!salaryData) {
+      salaryData = new Salary({
+        userId: user.id,
+        lastClaim: null,
+        salaryBlocked: false
+      });
+    } else {
+      salaryData.salaryBlocked = false;
+    }
 
-    message.reply(`${user.username} adlı kullanıcının bu haftaki maaş alım hakkı yeniden aktif edildi.`);
+    await salaryData.save();
+
+    message.reply(`${user.tag} kullanıcısının maaş alma hakkı açıldı.`);
   }
 };
