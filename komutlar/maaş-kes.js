bd7caea477790a3a450f6a19e4151ea062393eee
ixaryl <rahimasim15@gmail.com>
2025-06-16 14:45:23 +0400

    Update maaş-kes.js

--- komutlar/maaş-kes.js
+++ komutlar/maaş-kes.js
@@ -1,22 +1,34 @@
+const { PermissionsBitField } = require('discord.js');
 const Salary = require('../models/Salary');
 
 module.exports = {
   name: 'maaş-kes',
-  description: 'Kullanıcının maaş almasını engeller.',
+  description: 'Bir kullanıcının maaş alma hakkını engeller.',
   async execute(message, args) {
-    if (!message.member.permissions.has('ADMINISTRATOR')) {
+    // Yetki kontrolü (örneğin, yönetici olabilir)
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
       return message.reply('Bu komutu kullanmak için yönetici olmalısın.');
     }
 
     const user = message.mentions.users.first();
-    if (!user) return message.reply('Bir kullanıcı etiketlemelisin.');
+    if (!user) {
+      return message.reply('Lütfen maaşı kesilecek kullanıcıyı etiketle.');
+    }
 
-    await Salary.updateOne(
-      { _id: user.id },
-      { $set: { salaryBlocked: true } },
-      { upsert: true }
-    );
+    let salaryData = await Salary.findOne({ userId: user.id });
+    if (!salaryData) {
+      // Kullanıcıya ait kayıt yoksa yeni oluşturup engelle
+      salaryData = new Salary({
+        userId: user.id,
+        lastClaim: null,
+        salaryBlocked: true
+      });
+    } else {
+      salaryData.salaryBlocked = true;
+    }
 
-    message.reply(`${user.username} kullanıcısının maaşı bu hafta için kesildi.`);
+    await salaryData.save();
+
+    message.reply(`${user.tag} kullanıcısının maaş alma hakkı engellendi.`);
   }
 };
