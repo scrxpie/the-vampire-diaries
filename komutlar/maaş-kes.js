fc1797a855f166a1be16b550d520ef94adb7aac0
ixaryl <rahimasim15@gmail.com>
2025-06-16 17:06:33 +0400

    Update maaş-kes.js

--- komutlar/maaş-kes.js
+++ komutlar/maaş-kes.js
@@ -1,25 +1,33 @@
+const { PermissionsBitField } = require('discord.js');
 const Salary = require('../models/Salary');
 
 module.exports = {
   name: 'maaş-kes',
-  description: 'Bir kullanıcının maaşını keser.',
+  description: 'Bir kullanıcının maaşını bu hafta için keser.',
   usage: '.maaş-kes @kullanıcı',
   async execute(message, args) {
-    if (!message.member.permissions.has("ADMINISTRATOR")) {
+    // Yönetici yetkisi kontrolü
+    if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
       return message.reply('Bu komutu kullanmak için yönetici olmalısın.');
     }
 
+    // Kullanıcı etiketi al
     const user = message.mentions.users.first();
     if (!user) {
       return message.reply('Lütfen maaşı kesilecek kullanıcıyı etiketle.');
     }
 
+    // Veritabanında kullanıcının maaş kaydını ara
     let salaryData = await Salary.findById(user.id);
+
+    // Eğer kayıt yoksa yenisini oluştur
     if (!salaryData) {
       salaryData = new Salary({ _id: user.id });
     }
 
+    // Maaş engelini aktif et
     salaryData.salaryBlocked = true;
+
     await salaryData.save();
 
     message.channel.send(`${user} kullanıcısının maaşı bu hafta için kesildi.`);
