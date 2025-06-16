7e717bc6516e7fd6e1c5898e0cebd834f244aed4
ixaryl <rahimasim15@gmail.com>
2025-06-16 16:48:44 +0400

    Update Salary.js

--- models/Salary.js
+++ models/Salary.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const salarySchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
+  _id: { type: String, required: true },
   lastClaimed: { type: Date, default: null },
-  salaryBlocked: { type: Boolean, default: false }, // 💡 yeni alan
+  salaryBlocked: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Salary', salarySchema);
