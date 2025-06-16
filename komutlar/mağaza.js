16af8b23f6eee076e6f5d64bcec485979aaf7c2c
ixaryl <rahimasim15@gmail.com>
2025-06-16 17:38:45 +0400

    Update mağaza.js

--- komutlar/mağaza.js
+++ komutlar/mağaza.js
@@ -5,58 +5,58 @@ module.exports = {
     description: 'Mağaza kategorilerini gösterir.',
     execute(message) {
         const categories = {
-            "Araçlar": [
-                "Eski Model Araba - 20000$",
-                "Standart Araba - 30000$",
-                "Motosiklet - 18000$",
-                "Spor Araba - 45000$"
-            ],
-            "Evler": [
-                "**1+0 Apartman - 50000$**",
-                "**1+1 Apartman - 60000$**",
-                "**2+1 Apartman - 70000$**",
-                "**3+1 Apartman - 80000$**",
-                "**Müstakil Ev - 100000$**",
-                "**Dublex Ev - 150000$**",
-                "**Orman Evi - 125000$**",
-                "**Dağ Evi - 100000$**",
-                "**Villa - 200000$**",
-                
-            ],
-            "Doğaüstü Silahlar ve Aletler": [
-                "**Normal Mermi - 500$**\n> Standart mühimmat. Doğaüstü varlıklara geçici etki.",
-                "**Gümüş Mermi - 7500$**\n> Kurtadamlar için ölümcül. Zehir etkili.",
-                "**Kurtboğan - 1500$**\n> Doğaüstü varlıklara acı ve zayıflık verir.",
-                "**Kurtboğanlı Mermi - 7500$**\n> Kurtadamları iyileşemez hale getirir.",
-                "**Kurtboğanlı Gaz Bombası - 7500$**\n> Alan etkili. Nefes keser, yavaşlatır.",
-                "**Elektrikli Şok Cihazı - 5000$**\n> Kas kontrolünü geçici durdurur.",
-                "**Dağ Külü (5 kişilik) - 10000$**\n> Kitsune ve ruhani varlıklara karşı bariyer sağlar.",
-                "**Kurşun Tuzakları - 2500$**\n> Fiziksel zarar. İzinsiz girişe karşı savunma.",
-                "**Zincirler - 1500$**\n> Esir alma. Özel materyalli olabilir.",
-                "**Kurtboğanlı İğne - 3000$**\n> Sessiz ve zayıflatıcı saldırı.",
-                "**Zayıf Noktalar Kitabı - 15000$**\n> Tüm türlerin zayıflıkları.",
-                "**Işıklı Tuzak - 2500$**\n> Düşman yaklaşınca sinyal verir.",
-                "**Triskelion - 12000$**\n> Beta kurtlar için denge aracı.",
-                "**Druid Ritüel Kitabı - 10000$**\n> Koruma büyüleri ve ayinler.",
-                "**Bestiary - 8000$**\n> Doğaüstü varlıkların detaylı bilgisi.",
-                "**Banshee Günlüğü - 5000$**\n> Yaklaşan ölümlerin sezgileri."
-            ],
-            "Klasik ve Modern Silahlar": [
-                "**Tabanca - 10000$**\n> Hafif, hızlı müdahale için.",
-                "**Yay - 30000$**\n> Sessiz saldırılar için.",
-                "**Arbalet - 30000$**\n> Güçlü ve isabetli.",
-                "**Ok - 500$**\n> Zehirli/kutsal türleri mevcuttur.",
-                "**Kılıç/Katana - 12000$**\n> Yakın dövüş. Özel güçlü olabilir.",
-                "**Tüfek - 40000$**\n> Uzak menzil, yüksek hasar.",
-                "**Pompalı Tüfek - 40000$**\n> Yakın mesafe yıkım.",
-                "**Makineli - 50000$**\n> Seri atış, kalabalık hedefler."
-            ],
-            "Takılar": [
-                "Gün Işığı Takıları - 1000$",
-                "Ay Işığı Takıları - 2000$",
-                "Gilbert Yüzüğü - 2500$"
-            ]
-        };
+    "Araçlar": [
+        "Eski Model Araba - 30000$",
+        "Standart Araba - 40000$",
+        "Motosiklet - 20000$",
+        "Spor Araba - 50000$"
+    ],
+    "Evler": [
+        "**1+0 Apartman - 50000$**",
+        "**1+1 Apartman - 60000$**",
+        "**2+1 Apartman - 70000$**",
+        "**3+1 Apartman - 80000$**",
+        "**Müstakil Ev - 100000$**",
+        "**Dublex Ev - 150000$**",
+        "**Orman Evi - 125000$**",
+        "**Dağ Evi - 100000$**",
+        "**Villa - 200000$**"
+    ],
+    "Mermiler": [
+        "**Normal Mermi - 500$**\n> Standart mühimmat. Doğaüstü varlıklara geçici etki.",
+        "**Gümüş Mermi - 7500$**\n> Kurtadamlar için ölümcül. Zehir etkili.",
+        "**Sarı Kurtboğanlı Mermi - 7500$**\n> Kurtadamları iyileşemez hale getirir.",
+        "**Kurtboğanlı Mermi - 7500$**\n> Kurtadamları iyileşemez hale getirir.",
+        "**Ok - 500$**\n> Zehirli/kutsal türleri mevcuttur."
+    ],
+    "Tuzaklar": [
+        "**Elektrikli Şok Cihazı - 5000$**\n> Kas kontrolünü geçici durdurur.",
+        "**Kurşun Tuzakları - 2500$**\n> Fiziksel zarar. İzinsiz girişe karşı savunma.",
+        "**Zincirler - 1500$**\n> Esir alma. Özel materyalli olabilir.",
+        "**Işıklı Tuzak - 2500$**\n> Düşman yaklaşınca sinyal verir.",
+        "**Kurtboğanlı Gaz Bombası - 7500$**\n> Alan etkili. Nefes keser, yavaşlatır."
+    ],
+    "Aletler": [
+        "**Banshee Günlüğü - 5000$**\n> Yaklaşan ölümlerin sezgileri.",
+        "**Bestiary - 8000$**\n> Doğaüstü varlıkların detaylı bilgisi.",
+        "**Druid Ritüel Kitabı - 10000$**\n> Koruma büyüleri ve ayinler.",
+        "**Triskelion - 12000$**\n> Beta kurtlar için denge aracı.",
+        "**Kurtboğanlı İğne - 3000$**\n> Sessiz ve zayıflatıcı saldırı.",
+        "**Zayıf Noktalar Kitabı - 15000$**\n> Tüm türlerin zayıflıkları.",
+        "**Dağ Külü (5 kişilik) - 10000$**\n> Kitsune ve ruhani varlıklara karşı bariyer sağlar.",
+        "**Kurtboğan - 1500$**\n> Doğaüstü varlıklara acı ve zayıflık verir.",
+        "**Sarı Kurtboğan - 1500$**\n> Daha etkili kurtboğan türü."
+    ],
+    "Klasik ve Modern Silahlar": [
+        "**Tabanca - 10000$**\n> Hafif, hızlı müdahale için.",
+        "**Yay - 30000$**\n> Sessiz saldırılar için.",
+        "**Arbalet - 30000$**\n> Güçlü ve isabetli.",
+        "**Kılıç/Katana - 12000$**\n> Yakın dövüş. Özel güçlü olabilir.",
+        "**Tüfek - 40000$**\n> Uzak menzil, yüksek hasar.",
+        "**Pompalı Tüfek - 40000$**\n> Yakın mesafe yıkım.",
+        "**Makineli - 50000$**\n> Seri atış, kalabalık hedefler."
+    ]
+};
 
         const categoryImages = {
             "Araçlar": "https://i.imgur.com/5NjHuR0.gif",
