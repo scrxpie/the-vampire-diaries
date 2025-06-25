25b976be5a2a8269afb3438516b981cc002f9504
scrxpie <elsenn3345@gmail.com>
2025-06-25 19:57:31 +0400

    Update konuştur.js

--- komutlar/konuştur.js
+++ komutlar/konuştur.js
@@ -2,16 +2,20 @@ module.exports = {
     name: 'konuştur',
     description: 'Botun belirttiğiniz mesajı yazmasını sağlar.',
     execute(message, args) {
-      if (!message.member.permissions.has("ADMINISTRATOR")) {
-      return message.reply("Bu komutu kullanmak için yeterli yetkiniz yok.");
-    }
-        // Kullanıcının yazdığı metni al
+        if (!message.member.permissions.has("ADMINISTRATOR")) {
+            return message.reply("Bu komutu kullanmak için yeterli yetkiniz yok.");
+        }
+
         const metin = args.join(' ');
 
-        // Eğer metin boşsa hata mesajı döndür
         if (!metin) {
             return message.reply('Lütfen bir mesaj yazın! Örnek kullanım: `.konuştur selam`');
         }
+
+        // Kullanıcının komut mesajını sil
+        message.delete().catch(err => {
+            console.error("Mesaj silinemedi:", err);
+        });
 
         // Botun mesajı göndermesi
         message.channel.send(metin);
