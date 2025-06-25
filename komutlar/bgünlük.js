1d6a4ab9c69cce10d519a6415c372d3902d10f88
scrxpie <elsenn3345@gmail.com>
2025-06-25 18:53:11 +0400

    Update bgünlük.js

--- komutlar/bgünlük.js
+++ komutlar/bgünlük.js
@@ -12,7 +12,7 @@ module.exports = {
     name: 'bgünlük',
     description: 'Boosterlara özel günlük ödül komutu.',
     async execute(message) {
-        const boosterRoleId = '1370821728778846328';
+        const boosterRoleId = '1327637020767555626';
         const userId = message.author.id;
 
         if (!message.member.roles.cache.has(boosterRoleId)) {
