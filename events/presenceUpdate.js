02129de42b7955d9a27d2f87aaf4f8bc466f98ac
scrxpie <elsenn3345@gmail.com>
2025-07-01 15:11:56 +0400

    Create presenceUpdate.js

---
+++ events/presenceUpdate.js
@@ -0,0 +1,18 @@
+const { checkAndUpdateRole } = require('../utils/lanyardTagCheck');
+const GUILD_ID = '1327541284646293504
+const ROLE_ID = '1389547560682782731';
+
+module.exports = {
+  name: 'presenceUpdate',
+  async execute(oldPresence, newPresence, client) {
+    if (!newPresence || !newPresence.user) return;
+
+    // Sunucuda mı kontrol et
+    if (!newPresence.guild || newPresence.guild.id !== GUILD_ID) return;
+
+    // Kullanıcı id'si
+    const userId = newPresence.user.id;
+
+    await checkAndUpdateRole(client, GUILD_ID, ROLE_ID, userId);
+  }
+};
